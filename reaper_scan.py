7dfe329b4fc89f0e72674176ef46809e5ee794c1
Mark <mdemoraisgastaldi@gmail.com>
2026-09-02 15:01:00 -0300

    Prototipe, in dev

---
+++ reaper_scan.py
@@ -0,0 +1,17 @@
+import socket
+import os
+
+def limpar_tela():
+    os.system("cls")
+    
+# Escaneamento de portas
+def scan():
+    pass
+
+# Menu principal
+def menu():
+    pass
+
+# Opções gerais
+def opcoes():
+    pass
