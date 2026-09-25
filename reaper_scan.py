298155eacb06f77c053c13157a50c35fddafe3e0
Mark <mdemoraisgastaldi@gmail.com>
2026-09-25 07:37:10 -0300

    Reaper V0.1

--- reaper_scan.py
+++ reaper_scan.py
@@ -1,29 +1,40 @@
-# prototype
-# Reaper Port Scan
-# In dev
-
+# ██████╗ ███████╗ █████╗ ██████╗ ███████╗██████╗
+# ██╔══██╗██╔════╝██╔══██╗██╔══██╗██╔════╝██╔══██╗
+# ██████╔╝█████╗  ███████║██████╔╝█████╗  ██████╔╝
+# ██╔══██╗██╔══╝  ██╔══██║██╔═══╝ ██╔══╝  ██╔══██╗
+# ██║  ██║███████╗██║  ██║██║     ███████╗██║  ██║
+# ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚═╝     ╚══════╝╚═╝  ╚═╝
+# REAPER PORT SCANNER
+# Made By MarkDevBrasil
+# My GitHub: https://github.com/MarkDevBrasil
 
 import socket
+from concurrent.futures import ThreadPoolExecutor
 
+# Função para escanear uma porta específica em um alvo
+def scan_porta(alvo, porta):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(0.2)
 
+    resultado = s.connect_ex((alvo, porta))
+
+    if resultado == 0:
+        print(f"[+] Porta {porta}: ABERTA")
+    s.close()
+
+# Função para realizar o scan de portas em um alvo específico
 def scan():
-    alvo = input("Digite o ip do alvo: ")
-    portas = [21, 22, 80, 443, 3306, 8080]
+    alvo = input("Digite o IP do alvo: ")
+
+    portas = range(1, 65536)
 
     print("Iniciando o scan...")
 
-    for porta in portas:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    with ThreadPoolExecutor(max_workers=100) as executor:
+        for porta in portas:
+            executor.submit(scan_porta, alvo, porta)
 
-        s.settimeout(1)
+    print("Scan finalizado.")
 
-        resultado = s.connect_ex((alvo, porta))
-
-        if resultado == 0:
-            print(f"A porta {porta}: ABERTA")
-        else:
-            print(f"a porta {porta}: FECHADA")
-
-        s.close()
 
 scan()
